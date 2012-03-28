0637eae25ecfc42b0349772fbd2f5b709e98ee7e
Dmytro Kryvenko <dmytro.kryvenko@gmail.com>
2012-03-29 01:15:39 +0300

    added comments

--- src/test/java/DefaultSimpleStackTest.java
+++ src/test/java/DefaultSimpleStackTest.java
@@ -8,9 +8,9 @@ import static org.mockito.Mockito.*;
 
 public class DefaultSimpleStackTest<T> {
 
-	@SuppressWarnings({ "rawtypes" })
+	// create mock object which implements List interface
 	List mockList = mock(List.class);
-	@SuppressWarnings("unchecked")
+	
 	DefaultSimpleStack<Integer> stack = new DefaultSimpleStack<Integer>(mockList);
 
 	@Test(expected = IllegalArgumentException.class)
@@ -20,13 +20,18 @@ public class DefaultSimpleStackTest<T> {
 
 	@Test
 	public void testPush() {
+		// create argument captor for Integer class 
 		ArgumentCaptor<Integer> arg = ArgumentCaptor.forClass(Integer.class);
 
+		// call testing object
 		stack.push(5);
 		stack.push(3);
 		stack.push(7);
 
+		// verify that method add() was called third time and capture passed arguments
 		verify(mockList, times(3)).add(arg.capture());
+		
+		// get list of arguments which were passed to method add() and assert it
 		List<Integer> args = arg.getAllValues();
 		assertEquals(5, args.get(0).intValue());
 		assertEquals(3, args.get(1).intValue());
@@ -36,12 +41,13 @@ public class DefaultSimpleStackTest<T> {
 
 	@Test
 	public void testPop() {
-		// method size() will be called third times
+		// program behavior of methods size() and get() of mock list
 		when(mockList.size()).thenReturn(3).thenReturn(2).thenReturn(1);
 		when(mockList.get(2)).thenReturn(7);
 		when(mockList.get(1)).thenReturn(3);
 		when(mockList.get(0)).thenReturn(5);
 		
+		// call testing object and assert results
 		assertEquals(7, stack.pop().intValue());
 		assertEquals(3, stack.pop().intValue());
 		assertEquals(5, stack.pop().intValue());
